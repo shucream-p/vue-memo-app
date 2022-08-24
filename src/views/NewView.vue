6a682e49b702d35859977a174d3c63a5a8209427
tominaga <aurora25sh.50nonono@gmail.com>
2022-08-24 15:02:45 +0900

    新規メモページを作成

---
+++ src/views/NewView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>新規メモ</h2>
+    <div>
+      <textarea cols="40" rows="15"></textarea>
+    </div>
+    <div>
+      <input type="submit" value="保存">
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  textarea {
+    margin-bottom: 20px;
+  }
+</style>
