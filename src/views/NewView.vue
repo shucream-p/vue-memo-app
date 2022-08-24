3dec89fda5513bc227fd58f18a9afd29df3f3fbf
tominaga <aurora25sh.50nonono@gmail.com>
2022-08-24 18:19:11 +0900

    メモをローカルストレージに保存

--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -2,16 +2,37 @@
   <div>
     <h2>新規メモ</h2>
     <div>
-      <textarea cols="40" rows="15"></textarea>
+      <textarea cols="40" rows="15" v-model="newMemo"></textarea>
     </div>
     <div>
-      <input type="submit" value="保存">
+      <button @click.prevent="addMemo">保存</button>
     </div>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'NewView',
+  data() {
+    return {
+      newMemo: '',
+      memos: []
+    }
+  },
+  methods: {
+    addMemo() {
+      const title = this.newMemo.split('\n')[0]
+      const memo = {
+        id: new Date().getTime().toString(),
+        title: title,
+        content: this.newMemo
+      }
+      this.memos.push(memo)
+      this.newMemo = ''
+      localStorage.setItem('memos', JSON.stringify(this.memos))
+    }
+  }
+}
 </script>
 
 <style scoped>
