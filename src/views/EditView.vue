7a38df0cee221fd39cad93b27f7bb739b3673769
tominaga <aurora25sh.50nonono@gmail.com>
2022-08-29 13:28:47 +0900

    編集機能の修正と削除機能を追加

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,26 +1,51 @@
 <template>
 <div>
-  <div>
-    <textarea v-model="memos.find(element => element.id === this.$route.params.id).content"></textarea>
-  </div>
-  <div>
-      <button @click.prevent="editMemo">編集</button>
+  <div v-if="memo">
+    <div>
+      <textarea cols="40" rows="15" v-model="memo.content"></textarea>
+    </div>
+    <div>
+      <button @click="editMemo">編集</button>
+      <button @click="deleteMemo">削除</button>
+    </div>
   </div>
 </div>
 </template>
 
 <script>
 export default {
+  name: 'EditView',
   data () {
     return {
       memos: JSON.parse(localStorage.getItem('memos'))
+    }
+  },
+  computed: {
+    memo () {
+      return this.memos.find(memo => memo.id === this.$route.params.id)
     }
   },
   methods: {
     editMemo () {
       localStorage.setItem('memos', JSON.stringify(this.memos))
       this.$router.push('/')
+    },
+    deleteMemo () {
+      if (confirm('削除してよろしいですか？')) {
+        this.memos = this.memos.filter(memo => memo.id !== this.$route.params.id)
+        localStorage.setItem('memos', JSON.stringify(this.memos))
+        this.$router.push('/')
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+  textarea {
+    margin-bottom: 20px;
+  }
+  button {
+    margin: 0 5px;
+  }
+</style>
